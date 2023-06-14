77881b6eba639240191559ba71d80c33e0ee738e
Gökhan Erbil <gokhanerbil5@gmail.com>
2023-06-14 12:41:41 +0300

    Initial commit

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,17 @@
+package hooks;
+
+import io.cucumber.java.Before;
+
+import static base_urls.ManagementBaseUrl.setUp;
+
+public class Hooks {
+
+    @Before("@api")//Sadece parantez içinde belirtilen tag senaryoları öncesi çalışır
+    public void beforeApi(){
+
+        setUp();
+
+    }
+
+
+}
