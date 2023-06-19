81e0e6ca50022f48c9989ed709e5d45961378a9f
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-06-19 20:42:47 +0300

    ui

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,17 +1,31 @@
 package hooks;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
 
 import static base_urls.ManagementBaseUrl.setUp;
 
 public class Hooks {
 
-    @Before("@api")//Sadece parantez içinde belirtilen tag senaryoları öncesi çalışır
+    @Before("")//Sadece parantez içinde belirtilen tag senaryoları öncesi çalışır
     public void beforeApi(){
 
         setUp();
 
     }
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {//Eger fail olursa alınan ekran goruntusunu rapora ekle
+            final byte[] failedScreenShot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(failedScreenShot, "image/png", "failed_scnenaio" + scenario.getName());
+        }
+        Driver.closeDriver();
+    }
+
 
 
 }
--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -5,13 +5,21 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (
-
-        features="src/test/resources",
-        glue= {"stepDefinitions","hooks"},
-        tags="@",
-        dryRun= false
+@CucumberOptions(
+        plugin = {
+                "pretty",//konsolu renkli yazdirir
+                "html:target/default-cucumber-reports.html",//en ıyısı bu, en okunabiliri
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        monochrome = false,//konsolu okunakli hala getirir false olunca kullanılmayacak.
+        features = "src/test/resources",
+        glue = {"stepDefinitions","hooks"},
+        tags ="@ADMIN",
+        dryRun = false
 )
+
 
 public class Runner {
 
